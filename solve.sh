110a99cc24eb79f0d29bf1346acfb1079988357a
Alvaro Caceres <alvarocaceresmunoz@gmail.com>
2017-10-25 01:53:05 +0200

    included mathprog

--- solve.sh
+++ solve.sh
@@ -1,3 +1,2 @@
-# se asume que tienes la carpeta de mathprog un directorio por encima de la carpeta de github
-cd ../glpk-4.63/examples
-./glpsol -m ../../heuristics-lab-assignment-1/code/part-2/problem1.mod -d ../../heuristics-lab-assignment-1/code/part-2/problem1.dat -o ../../heuristics-lab-assignment-1/code/part-2/problem2.out
+#cd ./glpk-4.63/examples
+./glpsol -m ./code/part-2/problem1.mod -d ./code/part-2/problem1.dat -o ./code/part-2/problem2.out
