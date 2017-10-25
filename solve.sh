d958f152cc1aac69590c4328c0401687a311a300
Álvaro Cáceres Muñoz <100303602@alumnos.uc3m.es>
2017-10-25 00:02:29 +0000

    mathprog removed (too heavy to push to github); assumed mathprog is installed locally

--- solve.sh
+++ solve.sh
@@ -1,2 +1,2 @@
 #cd ./glpk-4.63/examples
-./glpsol -m ./code/part-2/problem1.mod -d ./code/part-2/problem1.dat -o ./code/part-2/problem2.out
+glpsol -m ./code/part-2/problem1.mod -d ./code/part-2/problem1.dat -o ./code/part-2/problem2.out
